8f3142c68acfbe8298157d41d0c30612e2acde6b
jlonetree <jlonetree@gmail.com>
2020-09-23 17:20:01 -0500

    Done.

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,34 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end 
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+
+    def create
+        @employee = Employee.new(employee_params(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id))
+
+        if !@employee.valid?
+            flash[:errors] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        else 
+            @employee.save
+            redirect_to @employee
+        end
+
+    end
+
+    def employee_params(*args)
+        params.require(:employee).permit(*args)
+    end
+
+    
 end
